3f08175d145ff67cb75d39cbb8a06f24d3e908e0
Handler <handler2104@gmail.com>
2025-03-18 19:30:26 +0100

    fix unexpected jncep crash because I'm too poor to buy maho LN

--- src/jncep_launcher.py
+++ src/jncep_launcher.py
@@ -10,6 +10,9 @@ def jncep_launcher(url) -> True:
     p = subprocess.run(cmd, capture_output = True, text = True, shell = True)
     print(p.stdout)
     print(p.stderr)
+    # dirty fix for poor people
+    if "No EPUB will be generated" in p.stdout:
+        return ""
     if p.returncode == 0:
         files = os.listdir(os.getcwd())
         return [i for i in files if i.endswith(".epub")][0]
